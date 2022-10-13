9f1debdae1a88b60deb73774f049fe422eb12dc3
leonardo mendoza <leoo_m@hotmail.com>
2022-10-13 19:17:09 -0300

    1era Versión, se realizo creación de clases y pagina Principal justo con el diseño web mas el listado de productos en una grilla

---
+++ CarritoCompras_LeonardoMendoza/Default.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CarritoCompras_LeonardoMendoza._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <asp:GridView runat="server" ID="dgvArticulos" CssClass="table-striped alert-warning table" AutoGenerateColumns ="true">  </asp:GridView>
+ 
+</asp:Content>
---
+++ CarritoCompras_LeonardoMendoza/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
+
+namespace CarritoCompras_LeonardoMendoza
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            dgvArticulos.DataSource = negocio.listar();
+            dgvArticulos.DataBind();   
+
+        }
+    }
+}
